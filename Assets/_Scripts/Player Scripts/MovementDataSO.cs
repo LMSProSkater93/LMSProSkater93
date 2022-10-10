e8ef263783384e02035ae900cd14de275ed1cd69
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2022-10-10 14:17:12 +0200

    Rettet PlayerObject-prefab til + nyt movement script

--- Assets/_Scripts/Player Scripts/MovementDataSO.cs
+++ Assets/_Scripts/Player Scripts/MovementDataSO.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Agent/MovementData")]
 public class MovementDataSO : ScriptableObject
 {
-    [Range(1, 10)] public float maxSpeed = 5;
+    [Range(1, 10)] public float maxSpeed = 10;
 
     [Range(0.1f, 100)] public float accelaration = 50, deaccelaration = 50;
 }
